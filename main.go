94e0ce791b3620d3c4ffe6c9fa825615e3bc6a16
kafrax <kafrax.go@gmail.com>
2017-11-27 14:23:22 +0800

    update4

--- main.go
+++ main.go
@@ -1,41 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"errors"
-	"sync"
-	"reflect"
-)
-type modelInfo struct {
-	typ          reflect.Type
-	name         string
-	secondary    string
-	expire       string
-	fieldIndex   map[string][]int
-	uniqueLength int
-	indexLength  int
-	defaultVal   string
-	sync.RWMutex
-}
+import "fmt"
+
 func main() {
-	var m=&modelInfo{}
-	fmt.Println(reflect.TypeOf(m).Elem())
-	fmt.Println(test())
+	fmt.Println("提交revision标记-------1")
 }
-
-func test() (ret int, err error) {
-	defer func() {
-		ret, ret1, err := test1()
-		fmt.Printf("%p\n   %p\n   %p\n", &ret, &ret1, &err)
-		return
-	}()
-	ret=123
-	err=errors.New("some err")
-	return
-}
-
-func test1() (ret, ret1 int, err error) {
-	fmt.Printf("%p\n   %p\n   %p\n", &ret, &ret1, &err)
-	return 0, 0, nil
-}
-
